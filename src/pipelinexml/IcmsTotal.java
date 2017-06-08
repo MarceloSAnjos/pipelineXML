e6b66269a4fe82c70f15ce340e7466b7a3186483
Thiago <thiagolmaeda@gmail.com>
2017-06-08 11:44:44 -0300

    Fim da implementacao do total

---
+++ src/pipelinexml/IcmsTotal.java
@@ -0,0 +1,200 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pipelinexml;
+
+/**
+ *
+ * @author maeda
+ */
+public class IcmsTotal {
+    
+    private String vBC;
+    private String vICMS;
+    private String vICMSDeson;
+    private String vFCPUFDest;
+    private String vICMSUFDest;
+    private String vICMSUFRemt;
+    private String vBCST;
+    private String vST;
+    private String vProd;
+    private String vFrete;
+    private String vSeg;
+    private String vDesc;
+    private String vII;
+    private String vIPI;
+    private String vPIS;
+    private String vCOFINS;
+    private String vOutro;
+    private String vNF;
+
+    public IcmsTotal(String vBC, String vICMS, String vICMSDeson, String vFCPUFDest, String vICMSUFDest, String vICMSUFRemt, String vBCST, String vST, String vProd, String vFrete, String vSeg, String vDesc, String vII, String vIPI, String vPIS, String vCOFINS, String vOutro, String vNF) {
+        this.vBC = vBC;
+        this.vICMS = vICMS;
+        this.vICMSDeson = vICMSDeson;
+        this.vFCPUFDest = vFCPUFDest;
+        this.vICMSUFDest = vICMSUFDest;
+        this.vICMSUFRemt = vICMSUFRemt;
+        this.vBCST = vBCST;
+        this.vST = vST;
+        this.vProd = vProd;
+        this.vFrete = vFrete;
+        this.vSeg = vSeg;
+        this.vDesc = vDesc;
+        this.vII = vII;
+        this.vIPI = vIPI;
+        this.vPIS = vPIS;
+        this.vCOFINS = vCOFINS;
+        this.vOutro = vOutro;
+        this.vNF = vNF;
+    }
+
+    public String getvBC() {
+        return vBC;
+    }
+
+    public void setvBC(String vBC) {
+        this.vBC = vBC;
+    }
+
+    public String getvICMS() {
+        return vICMS;
+    }
+
+    public void setvICMS(String vICMS) {
+        this.vICMS = vICMS;
+    }
+
+    public String getvICMSDeson() {
+        return vICMSDeson;
+    }
+
+    public void setvICMSDeson(String vICMSDeson) {
+        this.vICMSDeson = vICMSDeson;
+    }
+
+    public String getvFCPUFDest() {
+        return vFCPUFDest;
+    }
+
+    public void setvFCPUFDest(String vFCPUFDest) {
+        this.vFCPUFDest = vFCPUFDest;
+    }
+
+    public String getvICMSUFDest() {
+        return vICMSUFDest;
+    }
+
+    public void setvICMSUFDest(String vICMSUFDest) {
+        this.vICMSUFDest = vICMSUFDest;
+    }
+
+    public String getvICMSUFRemt() {
+        return vICMSUFRemt;
+    }
+
+    public void setvICMSUFRemt(String vICMSUFRemt) {
+        this.vICMSUFRemt = vICMSUFRemt;
+    }
+
+    public String getvBCST() {
+        return vBCST;
+    }
+
+    public void setvBCST(String vBCST) {
+        this.vBCST = vBCST;
+    }
+
+    public String getvST() {
+        return vST;
+    }
+
+    public void setvST(String vST) {
+        this.vST = vST;
+    }
+
+    public String getvProd() {
+        return vProd;
+    }
+
+    public void setvProd(String vProd) {
+        this.vProd = vProd;
+    }
+
+    public String getvFrete() {
+        return vFrete;
+    }
+
+    public void setvFrete(String vFrete) {
+        this.vFrete = vFrete;
+    }
+
+    public String getvSeg() {
+        return vSeg;
+    }
+
+    public void setvSeg(String vSeg) {
+        this.vSeg = vSeg;
+    }
+
+    public String getvDesc() {
+        return vDesc;
+    }
+
+    public void setvDesc(String vDesc) {
+        this.vDesc = vDesc;
+    }
+
+    public String getvII() {
+        return vII;
+    }
+
+    public void setvII(String vII) {
+        this.vII = vII;
+    }
+
+    public String getvIPI() {
+        return vIPI;
+    }
+
+    public void setvIPI(String vIPI) {
+        this.vIPI = vIPI;
+    }
+
+    public String getvPIS() {
+        return vPIS;
+    }
+
+    public void setvPIS(String vPIS) {
+        this.vPIS = vPIS;
+    }
+
+    public String getvCOFINS() {
+        return vCOFINS;
+    }
+
+    public void setvCOFINS(String vCOFINS) {
+        this.vCOFINS = vCOFINS;
+    }
+
+    public String getvOutro() {
+        return vOutro;
+    }
+
+    public void setvOutro(String vOutro) {
+        this.vOutro = vOutro;
+    }
+
+    public String getvNF() {
+        return vNF;
+    }
+
+    public void setvNF(String vNF) {
+        this.vNF = vNF;
+    }
+    
+    
+    
+}
---
+++ src/pipelinexml/TotalNFe.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pipelinexml;
+
+/**
+ *
+ * @author maeda
+ */
+public class TotalNFe {
+    
+    private IcmsTotal ICMSTot;
+
+    public TotalNFe(IcmsTotal ICMSTot) {
+        this.ICMSTot = ICMSTot;
+    }
+
+    public IcmsTotal getICMSTot() {
+        return ICMSTot;
+    }
+
+    public void setICMSTot(IcmsTotal ICMSTot) {
+        this.ICMSTot = ICMSTot;
+    }
+    
+    
+}
